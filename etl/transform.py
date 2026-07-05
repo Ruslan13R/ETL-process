729eedabc6b29a0328181380fcb40e9229e4f072
Ruslan13R <ruslan.han13@mail.ru>
2026-07-05 09:43:07 +0300

    Create transformer.py

---
+++ etl/transform.py
@@ -0,0 +1,33 @@
+class Transformer:
+    def transform(self, headers, rows):
+        records = []
+
+        for row in rows:
+            query = row[0]
+            url = row[1]
+
+            temp_date_of_metrics = {}
+
+            for head in range(2, len(headers)):
+                date, metric = headers[head].split('_')
+
+                if date not in temp_date_of_metrics:
+                    temp_date_of_metrics[date] = {}
+
+                temp_date_of_metrics[date][metric] = row[head]
+
+            for dt, mt in temp_date_of_metrics.items():
+                records.append(
+                    {
+                       'dt': dt,
+                        'query': query,
+                        'page_path': url,
+                        'shows': mt['shows'],
+                        'position': mt['position'],
+                        'demand': mt['demand'],
+                        'ctr': mt['ctr'],
+                        'clicks': mt['clicks']
+                    }
+                )
+
+        return records
