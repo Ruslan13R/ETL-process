8d13e85d0ed476df99f215f6694574f0814fe576
Ruslan13R <ruslan.han13@mail.ru>
2026-07-05 09:25:07 +0300

    Create extract.py

---
+++ etl/extract.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+
+
+class Extractor:
+    def exctract(self):
+        wd = load_workbook('../data/kirby-msk.xlsx')
+        ws = wd.active
+
+        list_of_rows = []
+
+        for row in ws.iter_rows(values_only=True):
+            list_of_rows.append(row)
+
+        headers = list_of_rows.pop(0)
+
+        return headers, list_of_rows
