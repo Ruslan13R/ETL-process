1720a5556c19bf1c91a31cdcae4940f146d9ea95
Ruslan <ruslan.han13@mail.ru>
2026-07-11 09:59:58 +0300

    Edit Dockerfile.

--- docker/db/create.sql
+++ docker/db/create.sql
@@ -22,3 +22,11 @@ CREATE TABLE IF NOT EXISTS ppl.webmaster_aggregated (
     position float8,
     clicks int4
 );
+
+ALTER TABLE ppl.webmaster_aggregated
+ADD CONSTRAINT uq_webmaster
+UNIQUE (dt, page_path, query, impressions, clicks);
+
+ALTER TABLE rdl.webm_excel
+ADD CONSTRAINT uq_webm
+UNIQUE (dt, page_path, query, impressions, clicks);
